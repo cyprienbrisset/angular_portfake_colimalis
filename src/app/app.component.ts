0bc24dd97f22cf0fe903657df8f813fc69dd2f25
Cyprien BRISSET <cyprien.brisset@documalis.com>
2023-07-07 15:04:25 +0200

    second commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import {Component, ElementRef, ViewChild} from '@angular/core';
+import {ApiService} from "./services/api.service";
+import {lastValueFrom} from "rxjs";
+import {DialogService, DynamicDialogRef} from "primeng/dynamicdialog";
+import {TrackingDialogComponent} from "./components/tracking-dialog/tracking-dialog.component";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +10,55 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+
   title = 'colimalis';
+  public shipid: string = ''
+
+  ref: DynamicDialogRef | undefined;
+
+  // @ts-ignore
+  @ViewChild('idship') myInput: ElementRef;
+
+  constructor(private api: ApiService, public dialogService: DialogService) {
+
+  }
+
+  private async getship(shipid: string) {
+    if (shipid != '') {
+      let res = await lastValueFrom(this.api.getShippingInfo(shipid))
+      console.log(res);
+      if (res.returnCode === 200) {
+        this.show(res)
+      } else {
+
+      }
+    }
+  }
+
+  public setFocus() {
+    this.myInput.nativeElement.focus();
+  }
+
+  public OnEnter(event: any) {
+    if (event.key === 'Enter') {
+      this.getship(this.shipid);
+    }
+  }
+
+  show(res: any) {
+    this.ref = this.dialogService.open(TrackingDialogComponent, {
+      header: 'Suivi du colis : ' + this.shipid,
+      data: res,
+      width: '70%',
+      contentStyle: { overflow: 'auto' },
+      baseZIndex: 10000,
+      maximizable: true
+    });
+
+    this.ref.onClose.subscribe(() => {
+    });
+
+    this.ref.onMaximize.subscribe((value) => {
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,40 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {ButtonModule} from "primeng/button";
+import {RippleModule} from "primeng/ripple";
+import {AvatarModule} from "primeng/avatar";
+import {HttpClientModule} from "@angular/common/http";
+import {ApiService} from "./services/api.service";
+import {FormsModule} from "@angular/forms";
+import { TrackingDialogComponent } from './components/tracking-dialog/tracking-dialog.component';
+import {TableModule} from "primeng/table";
+import {TagModule} from "primeng/tag";
+import { TrackingStateComponent } from './components/tracking-state/tracking-state.component';
+import {DialogService} from "primeng/dynamicdialog";
+import {DialogModule} from "primeng/dialog";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TrackingDialogComponent,
+    TrackingStateComponent,
   ],
   imports: [
+    HttpClientModule,
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    ButtonModule,
+    RippleModule,
+    AvatarModule,
+    FormsModule,
+    TableModule,
+    TagModule,
+    DialogModule
   ],
-  providers: [],
+  providers: [ApiService, DialogService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
